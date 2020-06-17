8fcd3153a83724472a1e082e584952bb026333ca
Leonardo <leonardosantosbran@gmail.com>
2020-06-17 20:44:56 -0300

    Se creo conexion en ModeloBD

--- CapaDeDatos/ModeloBD.vb
+++ CapaDeDatos/ModeloBD.vb
@@ -1,3 +1,27 @@
-﻿Public Class ModeloBD
+﻿Imports System.Data.Odbc
+
+Public MustInherit Class ModeloBD
+    Public Driver As String = ""
+    Public Usuario As String = ""
+    Public Password As String = ""
+    Public Puerto As String = ""
+    Public NombreDeBD As String = ""
+    Public Servidor As String = ""
+
+
+    Public Conexion As New OdbcConnection
+    '("DRIVER=" + Me.Driver + ";UID=" + Me.Usuario + ";PWD=" + Me.Password + ";PORT=" + Me.Puerto + ";DATABASE=" + Me.NombreDeBD + ";SERVER=" + Me.Servidor)
+    ("
+        DRIVER=" + Me.Driver +
+        ";UID=" + Me.Usuario +
+        ";PWD=" + Me.Password +
+        ";PORT=" + Me.Puerto +
+        ";DATABASE=" + Me.NombreDeBD +
+        ";SERVER=" + Me.Servidor
+    )
+
+    Public Comando As New OdbcCommand
+    Public Lector As OdbcDataReader
+
 
 End Class
