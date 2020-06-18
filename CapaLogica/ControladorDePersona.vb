ed0e87d043b60f9e8641d2528c1161ba159aa389
ESI BUCEO <esi-buceo@outlook.com>
2020-06-18 19:59:27 -0300

    se comenzo con controladorpersona

--- CapaLogica/ControladorDePersona.vb
+++ CapaLogica/ControladorDePersona.vb
@@ -1,3 +1,25 @@
-﻿Module ControladorDePersona
+﻿Imports CapaDeDatos
+
+Module ControladorDePersona
+
+    Public Sub InsertarPersona(id As String, nombre As String, apellido As String, email As String)
+        Dim p As New ModeloPersona
+        p.Id = id
+        p.Nombre = nombre
+        p.Apellido = apellido
+        p.Email = email
+
+        p.Insertar()
+
+    End Sub
+
+    Public Sub CambiarPersona(id As String, nombre As String, apellido As String, email As String)
+        Dim p As New ModeloPersona
+        p.Id = id
+        p.Nombre = nombre
+        p.Apellido = apellido
+        p.Email = email
+        p.Cambiar()
+    End Sub
 
 End Module
