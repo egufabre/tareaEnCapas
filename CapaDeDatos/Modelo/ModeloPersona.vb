15f846f8ea20973a6cbae013605ddf073f9e40d9
ESI BUCEO <esi-buceo@outlook.com>
2020-06-18 19:40:13 -0300

    se crearon las sentencias sql en ModeloPersona

--- CapaDeDatos/Modelo/ModeloPersona.vb
+++ CapaDeDatos/Modelo/ModeloPersona.vb
@@ -1,3 +1,35 @@
 ﻿Public Class ModeloPersona
+    Inherits ModeloBD
+
+    Public Id As String
+    Public Nombre As String
+    Public Apellido As String
+    Public Email As String
+
+    Public Sub Insertar()
+        Comando.CommandText = "INSERT INTO persona VALUES(" + Me.Id + ",'" + Me.Nombre + "','" + Me.Apellido + "','" + Me.Email + "')"
+        Comando.ExecuteNonQuery()
+    End Sub
+
+    Public Sub Cambiar()
+        Comando.CommandText = "UPDATE persona SET nombre = '" + Me.Nombre + "', apellido ='" + Me.Apellido + "', email = '" + Me.Email + "' WHERE id = " + Me.Id
+        Comando.ExecuteNonQuery()
+    End Sub
+
+    Public Sub Eliminar()
+        Comando.CommandText = "DELETE FROM persona WHERE id = " + Me.Id
+        Comando.ExecuteNonQuery()
+    End Sub
+
+    Public Function Contar()
+        Me.Comando.CommandText = "SELECT COUNT(*) FROM persona"
+        Return Me.Comando.ExecuteScalar().ToString()
+    End Function
+
+    Public Function Mostrar()
+        Me.Comando.CommandText = "SELECT * FROM persona"
+        Lector = Comando.ExecuteReader()
+        Return Lector
+    End Function
 
 End Class
